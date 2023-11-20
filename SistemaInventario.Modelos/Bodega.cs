72cb65f3111b5c98dccf4a9233708f2493e5ea1e
luiscalderonramirez <l.calderon@live.cl>
2023-11-20 15:56:37 -0300

    Configuracion del Proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre es requerido")]
+        [MaxLength(60, ErrorMessage = "Nombre debe ser maximo 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripcion es requerida")]
+        [MaxLength(100, ErrorMessage = "Descripcion debe ser maximo 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
